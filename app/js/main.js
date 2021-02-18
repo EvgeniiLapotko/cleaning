9e47587e65a95248109f958b112e804b2cc0ce9d
Evgenii <lapotko.e@yandex.ru>
2021-02-18 15:48:56 +0300

    added desktop version page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function () {
+    $(".wrapper-tabs .tab").on("click", function (event) {
+        var id = $(this).attr("data-id");
+        $(".wrapper-tabs").find(".tab-item").removeClass("active-tab").hide();
+        $(".wrapper-tabs .tabs").find(".tab").removeClass("active");
+        $(this).addClass("active");
+        $("#" + id)
+            .addClass("active-tab")
+            .fadeIn();
+        return false;
+    });
+
     $(".container-before-after").twentytwenty({
         no_overlay: true,
     });
@@ -12,4 +23,13 @@ $(function () {
         focusOnSelect: true,
         swipe: false,
     });
+
+    const btn = document.querySelector(".burger__menu-wrapper");
+    const menu = document.querySelector(".menu");
+
+    const oppenMenu = () => {
+        menu.classList.toggle("active");
+    };
+
+    btn.addEventListener("click", oppenMenu);
 });
